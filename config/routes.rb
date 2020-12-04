371948dd40d1d7b779999b69f9859c9ca15c0c0a
AkioMabuchi <mabuchiakio@gmail.com>
2020-12-04 13:28:08 +0900

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'home/top'
-  get 'home/ugdg201212'
+  root "home#top"
+  get "/ugdg201212", to: "home#ugdg201212"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
